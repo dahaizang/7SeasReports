6cdd7a692c0c3bf54136bcccbf8a5f96ca85e485
Dahai Zang <dahaizang@gmail.com>
2019-05-05 08:52:03 -0500

    Add latest views and exclude on-essential views

--- 7SeasReports.py
+++ 7SeasReports.py
@@ -4,6 +4,7 @@ def main():
     aReport = report.report()
 
     aReport.execute(reportName = "2019v1Member", table = "2019v1Member", excelFile = "./2019v1Member.xlsx")
+    aReport.execute(reportName="2019v1RegisteredWaitingPayment", table="2019v1RegisteredWaitingPayment", excelFile="./2019v1RegisteredWaitingPayment.xlsx")
     aReport.execute(reportName = "2019v1UserClassSelection", table="2019v1UserClassSelection", excelFile = "./2019v1UserClassSelection.xlsx")
     aReport.execute(reportName = "2019v1MemberClass", table="2019v1MemberClass", excelFile = "./2019v1MemberClass.xlsx")
     aReport.execute(reportName = "2019v1MemberClassCount", table="2019v1MemberClassCount", excelFile = "./2019v1MemberClassCount.xlsx")
