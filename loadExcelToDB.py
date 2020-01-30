919e1764d99abfd6cfc5289e3bd8fbadc01c94df
Dahai Zang <dahaizang@gmail.com>
2020-01-30 00:00:15 -0600

    Minor refinement

--- loadExcelToDB.py
+++ loadExcelToDB.py
@@ -2,18 +2,23 @@
 
 import xlrd
 import report
+import datetime
 
 class ExcelToMySQL :
-    def execute(self, table, excelFile):
+    def execute(self, table, excelFile, sheetName):
         r = report.report()
         r.connectToDB()
         r.getDBTableMetaData(table)
 #        r.loadExcelToMySQL("non_website_orders", "importExcel.xlsx")
         # Open the workbook and define the worksheet
-        book = xlrd.open_workbook("importExcel.xlsx")
+        book = xlrd.open_workbook(excelFile)
 
         #sheet = book.sheet_by_name("Client_Data")
-        sheet = book.sheet_by_index(0)
+
+        school_year = sheetName
+
+        sheet = book.sheet_by_name(sheetName)
+
 
         # Establish a MySQL Connection
         cnx = r.cnx
@@ -21,12 +26,16 @@ class ExcelToMySQL :
         # Get the cursor, which is used to traverse the database, line by line
         cursor = cnx.cursor()
 
+        #truncate target table first
+        cursor.execute("delete from e7se41220768516.non_website_orders where school_year = '" + school_year + "'")
+
         # Create the INSERT INTO sql query
         query = """
         INSERT
         INTO
         e7se41220768516.non_website_orders
         (sequence,
+         school_year,
          wechat_nickname,
          name,
          username,
@@ -37,7 +46,7 @@ class ExcelToMySQL :
          country,
          city,
          remark)
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
         """
 
         # Create a for loop to iterate through each row in the xls file, starting from row 2
@@ -54,21 +63,24 @@ class ExcelToMySQL :
             city = sheet.cell(r, 9).value
             remark = sheet.cell(r, 10).value
 
+            date_paid_as_datetime = datetime.datetime(*xlrd.xldate_as_tuple(date_paid, book.datemode))
+
             values = (sequence,
-            wechat_nickname,
-            name,
-            username,
-            email,
-            payment_amount,
-            payment_method,
-            date_paid,
-            country,
-            city,
-            remark)
+                school_year,
+                wechat_nickname,
+                name,
+                username,
+                email,
+                payment_amount,
+                payment_method,
+                date_paid_as_datetime,
+                country,
+                city,
+                remark)
+
 
             # Execute sql query
             cursor.execute(query, values)
-            cnx.commit()
 
         # Close the cursor
         cursor.close()
@@ -77,13 +89,15 @@ class ExcelToMySQL :
         cnx.commit()
 
         # Print results
-        print("Data imported successfully")
+        print("Data imported successfully for sheet '" + sheetName + "' in file '" + excelFile)
         columns = str(sheet.ncols)
         rows = str(sheet.nrows)
 
 def main():
     loader = ExcelToMySQL()
-    loader.execute("non_website_orders", "importExcel.xlsx")
+    #loader.execute("non_website_orders", "四海书海2020国内会费.xlsx", "2019") #"non_website_orders.xlsx")
+    loader.execute("non_website_orders", "四海书海2020国内会费.xlsx", "2020") #"non_website_orders.xlsx")
+
 
 if __name__ == "__main__":
     main()
